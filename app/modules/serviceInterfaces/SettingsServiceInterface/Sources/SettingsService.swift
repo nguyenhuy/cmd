41cf5fe9ed5618d6f4e4f67916a2342b38219069
Guillaume Sabran <sabranguillaume@gmail.com>
2025-05-28 12:03:19 -0700

    fix remaining tests

--- app/modules/serviceInterfaces/SettingsServiceInterface/Sources/SettingsService.swift
+++ app/modules/serviceInterfaces/SettingsServiceInterface/Sources/SettingsService.swift
@@ -14,6 +14,7 @@ public struct Settings: Sendable, Equatable {
   public init(
     pointReleaseXcodeExtensionToDebugApp: Bool,
     allowAnonymousAnalytics: Bool = false,
+<<<<<<< HEAD
     preferedProviders: [LLMModel: LLMProvider] = [:],
     llmProviderSettings: [LLMProvider: LLMProviderSettings] = [:],
     inactiveModels: [LLMModel] = [])
@@ -23,6 +24,36 @@ public struct Settings: Sendable, Equatable {
     self.preferedProviders = preferedProviders
     self.llmProviderSettings = llmProviderSettings
     self.inactiveModels = inactiveModels
+=======
+    anthropicSettings: LLMProviderSettings?,
+    openAISettings: LLMProviderSettings?,
+    openRouterSettings: LLMProviderSettings? = nil,
+    googleAISettings _: LLMProviderSettings? = nil,
+    cohereSettings _: LLMProviderSettings? = nil)
+  {
+    self.pointReleaseXcodeExtensionToDebugApp = pointReleaseXcodeExtensionToDebugApp
+    self.allowAnonymousAnalytics = allowAnonymousAnalytics
+    self.anthropicSettings = anthropicSettings
+    self.openAISettings = openAISettings
+    self.openRouterSettings = openRouterSettings
+  }
+  #endif
+
+  public init(from decoder: any Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    pointReleaseXcodeExtensionToDebugApp = try container.decodeIfPresent(
+      Bool.self,
+      forKey: .pointReleaseXcodeExtensionToDebugApp) ?? false
+    #if DEBUG
+    allowAnonymousAnalytics = try container.decodeIfPresent(Bool.self, forKey: .allowAnonymousAnalytics) ?? true
+    #else
+    allowAnonymousAnalytics = try container.decodeIfPresent(Bool.self, forKey: .allowAnonymousAnalytics) ?? false
+    #endif
+    allowAnonymousAnalytics = try container.decodeIfPresent(Bool.self, forKey: .allowAnonymousAnalytics) ?? true
+    anthropicSettings = try container.decodeIfPresent(Settings.LLMProviderSettings.self, forKey: .anthropicSettings)
+    openAISettings = try container.decodeIfPresent(Settings.LLMProviderSettings.self, forKey: .openAISettings)
+    openRouterSettings = try container.decodeIfPresent(Settings.LLMProviderSettings.self, forKey: .openRouterSettings)
+>>>>>>> 9cca109 (fix remaining tests)
   }
 
   public struct LLMProviderSettings: Sendable, Codable, Equatable {
@@ -45,11 +76,34 @@ public struct Settings: Sendable, Equatable {
 
   public var allowAnonymousAnalytics: Bool
   public var pointReleaseXcodeExtensionToDebugApp: Bool
+<<<<<<< HEAD
   // LLM settings
   public var preferedProviders: [LLMModel: LLMProvider]
   public var llmProviderSettings: [LLMProvider: LLMProviderSettings]
 
   public var inactiveModels: [LLMModel]
+=======
+  public var anthropicSettings: LLMProviderSettings?
+  public var openAISettings: LLMProviderSettings?
+  public var openRouterSettings: LLMProviderSettings?
+
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    try container.encode(pointReleaseXcodeExtensionToDebugApp, forKey: .pointReleaseXcodeExtensionToDebugApp)
+    try container.encode(allowAnonymousAnalytics, forKey: .allowAnonymousAnalytics)
+    try container.encodeIfPresent(anthropicSettings, forKey: .anthropicSettings)
+    try container.encodeIfPresent(openAISettings, forKey: .openAISettings)
+    try container.encodeIfPresent(openRouterSettings, forKey: .openRouterSettings)
+  }
+
+  private enum CodingKeys: String, CodingKey {
+    case pointReleaseXcodeExtensionToDebugApp
+    case allowAnonymousAnalytics
+    case anthropicSettings
+    case openAISettings
+    case openRouterSettings
+  }
+>>>>>>> 9cca109 (fix remaining tests)
 
 }
 
